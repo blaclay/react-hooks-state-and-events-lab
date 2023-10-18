dfdcacdfcee72aab450cf2f8917fa32536ab956f
Clay Blackwelder <clayblackwelder@gmail.com>
2023-10-17 17:10:42 -0700

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [selectedCategory, setSelectedCategory] = useState(false);
+
+  function handleClick() {
+    setSelectedCategory((selectedCategory) => !selectedCategory);
+  }
+
   return (
-    <li className="">
+    <li className={selectedCategory ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className={selectedCategory ? "remove" : "add"}
+        onClick={handleClick}
+      >
+        {selectedCategory ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
